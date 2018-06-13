148597eb9115577c2bca2fbc00934693ac252a84
kui wu <kwu@cs.utah.edu>
2018-06-13 15:43:36 -0700

    add license

--- src/core/dual.cpp
+++ src/core/dual.cpp
@@ -1,10 +1,20 @@
-//
-//  Dual.cpp
-//  Single_Strip_Mesh_Simplification
-//
-//  Created by kui wu on 2/21/13.
-//  Copyright (c) 2013 kui wu. All rights reserved.
-//
+/*********************************************************************************
+*   Copyright (C) 2018 by Kui Wu                                                 *
+*   kwu@cs.utah.edu                                                              *
+*                                                                                *
+*   This program is free software: you can redistribute it and/or modify         *
+*   it under the terms of the GNU Lesser General Public License as published by  *
+*   the Free Software Foundation, either version 3 of the License, or	         *
+*   (at your option) any later version.									         *
+*   																	         *
+*   This program is distributed in the hope that it will be useful,		         *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of		         *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		         *
+*   GNU Lesser General Public License for more details.						     *
+*   																	         *
+*   You should have received a copy of the GNU Lesser General Public License	 *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.        *
+*********************************************************************************/
 
 #include "Dual.h"
 
